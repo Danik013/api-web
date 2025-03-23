ec730ae37f7217392ae631a166fb99cfef428c51
Danik013 <2077647@gmail.com>
2025-03-23 20:40:23 +0300

    Add files via upload

--- API_02.py
+++ API_02.py
@@ -6,17 +6,17 @@ from urllib.parse import urlparse
 
 def is_shorten_link(user_input, token):
     url_elements = urlparse(user_input)
-    domaine_name = url_elements.path
+    path_url = url_elements.path
     vk_stats = "https://api.vk.ru/method/utils.getLinkStats"
     params = {
         "access_token": token,
         "v": 5.199,
-        "key": domaine_name.replace("/", "", 1)
+        "key": path_url.replace("/", "", 1)
     }
     response = requests.get(vk_stats, params=params)
     response.raise_for_status()
-    keys_response = response.json()
-    return "response" in keys_response
+    link_info = response.json()
+    return "response" in link_info
 
 
 def shorten_link(token, base_url):
@@ -29,14 +29,12 @@ def shorten_link(token, base_url):
     }
     response = requests.get(vk_url, params=params)
     response.raise_for_status()
-    keys_response = response.json()
-    short_link = keys_response["response"]["short_url"]
+    link_info = response.json()
+    short_link = link_info["response"]["short_url"]
     return short_link
     
 
 def count_clicks(token, shorten_url):    
-    shorten_url_response = requests.get(shorten_url)
-    shorten_url_response.raise_for_status()
     url_elements = urlparse(shorten_url)
     domaine_name = url_elements.path
     vk_stats = "https://api.vk.ru/method/utils.getLinkStats"
@@ -47,14 +45,14 @@ def count_clicks(token, shorten_url):
     }
     response = requests.get(vk_stats, params=params)
     response.raise_for_status()
-    keys_response = response.json()
-    clicks_count = keys_response["response"]["stats"][0]["views"]
+    link_info = response.json()
+    clicks_count = link_info["response"]["stats"][0]["views"]
     return clicks_count
 
 
 def main():
     load_dotenv()
-    token = os.environ.get("VK_SERVICE_KEY", "KeyError: 'SOME_KEY'")
+    token = os.environ["VK_SERVICE_KEY"]
     user_input = input("Введите ссылку: ")
     if is_shorten_link(user_input, token):
         try:
@@ -66,12 +64,12 @@ def main():
         try:
             shorten_url = shorten_link(token, user_input)
             print("Сокращенная ссылка: ", shorten_url)
-        except requests.exceptions.HTTPError:
-            print("Вы ввели неправильную ссылку или параметры!")
-        except requests.exceptions.ConnectionError:
-            print("Ошибка соединения при попытке получить данные.")
-        except requests.exceptions.Timeout:
-            print("Время ожидания истекло.")
+        except requests.exceptions.HTTPError as http_err:
+            print(f"Вы ввели неправильную ссылку или параметры. Ошибка: {http_err}")
+        except requests.exceptions.ConnectionError as err:
+            print(f"Ошибка соединения: {err}")
+        except requests.exceptions.Timeout as time_err:
+            print(f"Время ожидания истекло: {time_err}")
         except requests.exceptions.MissingSchema:
             print(f"Недопустимый URL-адрес. Возможно вы имели ввиду: https://{user_input}")
     
